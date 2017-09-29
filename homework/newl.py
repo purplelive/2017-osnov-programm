e244d580b8d0ebf97f5271e4885b8784bb8dbe4f
sevilay <sevilaybayatli@gmail.com>
2017-09-29 10:55:19 -0700

    add tokeniser

--- homework/newl.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-line = sys.stdin.readline()
-while line: 
-	sys.stdout.write(line)
-	line = sys.stdin.readline()
--- homework/segment.py
+++ homework/segment.py
@@ -1,9 +1,8 @@
 import sys
-import re
+
 text = sys.stdin.read()
 
 for c in text:
-
-    text = text.replace('. ' ,'.\n')
+    text= text.replace('. ', '.\n')
     print(text)
-                
+    break
